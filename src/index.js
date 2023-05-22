28d69e9f4efabeed991f623b9f887a07d71fe7c8
ChristianStander <stander.christian@gmail.com>
2023-05-22 09:38:30 +0200

    a

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import Provider
+// import Provider and store
 import { Provider } from 'react-redux'
 import store from './store/store'
 import reportWebVitals from './reportWebVitals';
